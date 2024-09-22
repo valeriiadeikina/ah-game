a9e4a6d9b1d4ba12c173ba3a1f53a08f2c8d2e1b
Vallydei <obedkova.v@rambler.ru>
2024-09-22 22:03:32 +0300

    что-то

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,19 @@
-export default {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: "eslint:recommended",
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: "module",
-  },
-  rules: {
-    // Добавьте или измените правила по вашему усмотрению
-    "no-unused-vars": "warn", // Предупреждение о неиспользуемых переменных
-    "no-console": "off", // Отключение ошибки на console.log
-  },
-};
+import { defineConfig } from 'eslint-define-config';
+
+export default defineConfig({
+    languageOptions: {
+        globals: {
+            // Здесь можно определить глобальные переменные
+            window: 'readonly',
+            document: 'readonly',
+        },
+        parserOptions: {
+            ecmaVersion: 2021,
+            sourceType: 'module',
+        },
+    },
+    rules: {
+        'no-unused-vars': 'warn', // Предупреждение о неиспользуемых переменных
+        'no-console': 'off',      // Отключение ошибки на console.log
+    },
+});
