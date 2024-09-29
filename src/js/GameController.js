3ce6e3613a72d00b5b7bb8a019ac9e81161a038a
Vallydei <obedkova.v@rambler.ru>
2024-09-29 15:07:05 +0300

    save

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -1,14 +1,19 @@
 import goblinImage from "../img/goblin.png";
+import GamePanel from "./GamePanel.js";
 
 export default class GameController {
   constructor() {
     this.prevCellIndex = null;
+    this.gamePanel = null;
+    this.interval = null;
   }
 
   init() {
     document.addEventListener("DOMContentLoaded", () => {
       const container = document.querySelector("#game-container");
 
+      this.gamePanel = new GamePanel();
+      container.appendChild(this.gamePanel.drawPanel());
       const field = document.createElement("div");
       field.classList.add("game-field");
       for (let i = 0; i < 16; i++) {
@@ -16,6 +21,8 @@ export default class GameController {
         cell.classList.add("game-cell");
         cell.setAttribute("data-index", i);
         field.appendChild(cell);
+        cell.addEventListener("mouseenter", (event) => this.onCellEnter(event));
+        cell.addEventListener("click", (event) => this.onCellClick(event));
       }
       container.appendChild(field);
 
@@ -23,7 +30,7 @@ export default class GameController {
       enemy.classList.add("img");
       enemy.setAttribute("src", goblinImage);
       const allCellsCollection = document.querySelectorAll(".game-cell");
-      setInterval(() => {
+      this.interval = setInterval(() => {
         const randomCell = Math.floor(
           Math.random() * allCellsCollection.length
         );
@@ -34,7 +41,39 @@ export default class GameController {
           this.prevCellIndex = randomCell;
           targetCell.appendChild(enemy);
         }
-      }, 2000);
+      }, 1000);
     });
+  }
+
+  onCellEnter(e) {
+    if (e.target.children.length) {
+      e.target.classList.add("custom-cursor");
+    }
+  }
+
+  onCellClick(e) {
+    if (e.currentTarget.children.length) {
+      this.gamePanel.points += 1;
+      const removedEnemy = document.querySelector(".img");
+      removedEnemy.remove();
+      this.gamePanel.updatePanel();
+    } else {
+      this.gamePanel.mistakes += 1;
+      this.gamePanel.updatePanel();
+    }
+    this.gameOver();
+  }
+
+  gameOver() {
+    if (this.gamePanel.mistakes >= 5) {
+      const field = document.querySelector(".game-field");
+      field.remove();
+      const gameOverMessage = document.createElement("h1");
+      gameOverMessage.textContent = "Игра окончена!";
+      gameOverMessage.classList.add("game-over");
+      const container = document.querySelector("#game-container");
+      container.appendChild(gameOverMessage);
+      clearInterval(this.interval);
+    }
   }
 }
